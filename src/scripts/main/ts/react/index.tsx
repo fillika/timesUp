e0f37da79619f0e8c83c187d3085c72a2dc28158
Aleksandr <alfie.mir@gmail.com>
2021-05-04 14:41:57 +0300

    Добавил базовую верстку. Удалил лишний файл стилей

--- src/scripts/main/ts/react/index.tsx
+++ src/scripts/main/ts/react/index.tsx
@@ -6,12 +6,23 @@ import imgSvg from 'Images/webpack-logo.svg';
 import imgPng from 'Images/download.png';
 
 const App: React.FC = () => {
-  return <div>Hello from React</div>;
+  return (
+    <div>
+      <div>
+        <form>
+          <input type='text' placeholder='name' value='Проект номер 1' />
+          <div>
+            <button>Start/Stop</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('ts-main-react')
+  document.getElementById('app')
 );
