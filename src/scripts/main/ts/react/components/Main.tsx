477561b37f0259da17ea49ac9e79f8f22b08da48
Aleksandr M <alfie.mir@gmail.com>
2021-06-15 18:07:27 +0300

    Настроил получение тасков из базы данных

--- src/scripts/main/ts/react/components/Main.tsx
+++ src/scripts/main/ts/react/components/Main.tsx
@@ -2,24 +2,26 @@ import React, { useEffect, useState } from 'react';
 import Task from './Task';
 
 const Main: React.FC = () => {
-  const [data2, setData] = useState([]);
-  const url = 'http://localhost:22222/api/tasks';
+  const [taskArr, setTasks] = useState([]);
+  const url = 'http://localhost:22222/api/v1/tasks';
 
   useEffect(() => {
     const result = async () => {
-      const response = await getData(url);
-      setData(response);
+      try {
+        const response = await getData(url);
+        setTasks(response.data.tasks);
+      } catch (error) {
+        console.error(error);
+      }
     };
 
     result();
   }, []);
 
-  console.log(data2);
-
   return (
     <main className='main'>
       <ul className='task-list'>
-        {data2.map((tasks, index) => (
+        {taskArr.map((tasks, index) => (
           <Task tasks={tasks} key={index} />
         ))}
       </ul>
@@ -30,13 +32,13 @@ const Main: React.FC = () => {
 export default Main;
 
 async function getData(url: string, data = {}): Promise<any> {
-  const headers: RequestInit = {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  };
-  const response = await fetch(url, headers);
+  // const headers: RequestInit = {
+  //   method: 'GET', // *GET, POST, PUT, DELETE, etc.
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data), // body data type must match "Content-Type" header
+  // };
+  const response = await fetch(url);
   return response.json();
 }
