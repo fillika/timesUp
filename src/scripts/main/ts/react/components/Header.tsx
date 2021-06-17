d9d4af30be2fc104e0e3771ec0229608b74e8064
Aleksandr M <alfie.mir@gmail.com>
2021-06-17 10:04:39 +0300

    Добавил иконки для header. Добавил файл стилей, немного поправил стили (цвета).

--- src/scripts/main/ts/react/components/Header.tsx
+++ src/scripts/main/ts/react/components/Header.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import playBtn from 'Images/icons/play.svg';
+import stopBtn from 'Images/icons/stop-button.svg';
 
 const Header: React.FC = () => {
+  const [state, setstate] = useState(false);
+
+  const onClick = () => {
+    setstate(!state);
+  };
+
   return (
     <header className='header'>
       <div className='header__input-wrapper'>
         <input className='header__input' type='text' placeholder='Напишите задачу' />
       </div>
 
-      <div className="header__panel">
+      <div className='header__panel header-panel'>
+        <div>
+          <div>00:00:05</div>
+        </div>
         <div className='header__button-wrapper'>
-          <button className="header__button header__button--play">Start/Stop</button>
+          <button onClick={onClick} className='header__button header__button--play'>
+            <img src={!state ? playBtn : stopBtn} alt='Иконка' />
+          </button>
         </div>
       </div>
     </header>
--- src/scripts/main/ts/react/index.tsx
+++ src/scripts/main/ts/react/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from '../store';
-import imgSvg from 'Images/webpack-logo.svg';
-import imgPng from 'Images/download.png';
 import App from './App';
 
 ReactDOM.render(
