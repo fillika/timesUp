1af161c13a67860adc8dda7e76c66653937f5c9a
Aleksandr M <alfie.mir@gmail.com>
2021-06-23 09:59:57 +0300

    Настроил вывод общего времени для одного дня

--- src/scripts/main/ts/react/components/Main/Main.tsx
+++ src/scripts/main/ts/react/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Task from '../Task/Task';
 import { useGetTasks } from './useGetTasks';
+import { time } from 'Utils/Time';
 
 const Main: React.FC = () => {
   const taskArr = useGetTasks();
@@ -9,6 +10,13 @@ const Main: React.FC = () => {
     <main className='main'>
       {taskArr.map(({ date, dateISO, tasks }) => {
         const dateString = new Date(dateISO).toUTCString().slice(0, 12);
+        // Todo вынести логику из компонента
+        const getTotalDayTime = () => {
+          let result = 0;
+          tasks.forEach(el => (result += el.duration));
+          return time.countTotalTime(result);
+        };
+        const totalDayTime = getTotalDayTime();
 
         if (!tasks.length) {
           return null;
@@ -18,7 +26,10 @@ const Main: React.FC = () => {
           <div className='task-section' key={date}>
             <div className='task-section__wrapper'>
               <div>{dateString}</div>
-              <div>...</div>
+              <div className="task-section__panel">
+                <div className="task-section__total-time">{totalDayTime}</div>
+                <div className="task-section__menu">...</div>
+              </div>
             </div>
 
             <ul className='task-list'>
