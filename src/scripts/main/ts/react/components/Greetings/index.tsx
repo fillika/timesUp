e9f74a1ded93041f3932ac95e3e47764180e41c4
Aleksandr Mironenko <alfie.mir@gmail.com>
2021-06-27 20:08:45 +0300

    Создал форму и 2 блока для стилей. Создал черновую верстку и событие submit для формы.

--- src/scripts/main/ts/react/components/Greetings/index.tsx
+++ src/scripts/main/ts/react/components/Greetings/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 
 const Greetings = () => {
+  const submit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log('SUBMIT');
+  };
+
   return (
-    <div className="greeting">
-      <div>Добрый день! Вы к кому?</div>
+    <div className='greetings'>
+      <div>
+        <form onSubmit={submit} className='form'>
+          <div className='form__fields-wrapper'>
+            <label htmlFor='username'>Username</label>
+            <input type='text' name='username' id='username' placeholder='Username' />
+          </div>
+          <div className='form__fields-wrapper'>
+            <label htmlFor='email'>Email</label>
+            <input type='email' name='email' id='email' placeholder='Email' />
+          </div>
+          <div className='form__fields-wrapper'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' name='password' id='password' />
+          </div>
+          <div className='form__fields-wrapper'>
+            <label htmlFor='confirmPassword'>Confirm password</label>
+            <input type='password' name='confirmPassword' id='confirmPassword' />
+          </div>
+          <div>
+            <button>Регистрация</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
