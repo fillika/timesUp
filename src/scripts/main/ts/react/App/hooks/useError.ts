b8f25402f8eb57705bc2c6e9672e72b2396f34cf
Aleksandr M <alfie.mir@gmail.com>
2021-06-30 13:05:54 +0300

    Добавил обработку ошибки 401

--- src/scripts/main/ts/react/App/hooks/useError.ts
+++ src/scripts/main/ts/react/App/hooks/useError.ts
@@ -4,13 +4,24 @@ import { createNotify } from 'Utils/helpers/createNotify';
 
 export function useError() {
   const dispatch = useDispatch();
-  const message = 'Ошибка подключения к серверу. Приносим свои извинения :(';
+  let message = 'Ошибка подключения к серверу. Приносим свои извинения :(';
 
   const errHandler = (err: AppError) => {
-    if (err.statusCode === 404) {
-      dispatch({ type: 'APP_LOG_OUT' });
-      createNotify('error', message, dispatch);
+    switch (err.statusCode) {
+      case 401:
+        message = 'Пожалуйста, залогиньтесь заново';
+        createNotify('warning', message, dispatch);
+        break;
+      case 404:
+        message = 'Ошибка подключения к серверу. Приносим свои извинения :(';
+        createNotify('error', message, dispatch);
+        break;
+    
+      default:
+        break;
     }
+
+    dispatch({ type: 'APP_LOG_OUT' });
   };
 
   return [errHandler];
