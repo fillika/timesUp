8fc3b5f93d57f52fbbd834bb88678f9cf02505bd
Aleksandr M <alfie.mir@gmail.com>
2021-07-02 17:49:54 +0300

    Реализовал функцию log out

--- src/scripts/main/ts/react/components/Sidebar/index.tsx
+++ src/scripts/main/ts/react/components/Sidebar/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
+import { useDispatch } from 'react-redux';
 
 const Sidebar: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const logOut = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    localStorage.removeItem('JWT');
+    dispatch({ type: 'APP_LOG_OUT' });
+  };
+
   return (
-    <aside className="sidebar">
-      <p>Left menu</p>
+    <aside className='sidebar'>
+      <p>Track</p>
       <ul>
         <li>Timer</li>
       </ul>
+      <p>Statictis</p>
+      <ul>
+        <li>Reports</li>
+      </ul>
+      <p>Menu</p>
+      <ul>
+        <li>Profile</li>
+      </ul>
+
+      <a href='#' onClick={logOut}>
+        Log out
+      </a>
     </aside>
   );
 };
