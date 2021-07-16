09c869852918f91e2ebbe9c62285cf0cd448c498
A. Mironenko <alfie.front@gmail.com>
2021-07-16 12:46:47 +0300

    Вернул логику в useHeader

--- src/scripts/main/ts/react/components/Header/hooks/useHeader.ts
+++ src/scripts/main/ts/react/components/Header/hooks/useHeader.ts
@@ -1,20 +1,21 @@
-import { useEffect, ChangeEvent, KeyboardEvent, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useEffect } from 'react';
+import { useDispatch, useSelector, useStore } from 'react-redux';
 import { RootState } from 'Redux/reducers/rootReducer';
-import { time } from 'Utils/Time';
-import { taskHandler } from 'Utils/TaskHandler';
-import { createTask } from './../utils/createTask';
-import { getActiveTask } from '../utils/getActiveTask';
 import { useGlobalError } from 'App/hooks/useGlobalError';
+import { taskHandler } from 'Utils/TaskHandler';
+import { time } from 'Utils/Time';
+import { getActiveTask } from '../utils/getActiveTask';
+import { createTask } from '../utils/createTask';
 import _ from 'lodash';
 
 export function useHeader() {
   const dispatch = useDispatch();
   const {
-    activeTask,
+    activeTask: { isTimeActive, duration, start, name, totalTime },
     app: { token },
   } = useSelector((state: RootState) => state);
   const { activeTaskErrorHandler, createTaskErrorHandler } = useGlobalError();
+  const store = useStore();
 
   console.log('Render[Header]');
 
@@ -26,28 +27,28 @@ export function useHeader() {
 
   useEffect(() => {
     if (token) {
-      if (activeTask.isTimeActive) {
-        taskHandler.updateActiveTask(token, activeTask);
+      if (isTimeActive) {
+        taskHandler.updateActiveTask(token, store.getState().activeTask);
       }
     }
-  }, [activeTask.isTimeActive]);
+  }, [isTimeActive]);
 
   useEffect(() => {
-    if (activeTask.duration > 0 && token) {
-      createTask(createTaskErrorHandler, activeTask, dispatch, token);
+    if (duration > 0 && token) {
+      createTask(createTaskErrorHandler, store.getState().activeTask, dispatch, token);
       dispatch({ type: 'RESET_ACTIVE_TASK_PROPS', payload: { totalTime: '00:00:00', name: '', duration: 0 } });
       document.title = `TimesUp`;
     }
-  }, [activeTask.duration]);
+  }, [duration]);
 
   useEffect(() => {
     let timeoutID = setTimeout(() => {
-      if (activeTask.isTimeActive) {
-        const diff = new Date().getTime() - new Date(activeTask.start).getTime();
+      if (isTimeActive) {
+        const diff = new Date().getTime() - new Date(start).getTime();
         const totalTime = time.countTotalTime(diff);
         dispatch({ type: 'SET_ACTIVE_TASK_TOTAL_TIME', payload: totalTime });
 
-        document.title = `${totalTime} - ${activeTask.name}`;
+        document.title = `${totalTime} - ${name}`;
       } else {
         clearTimeout(timeoutID);
       }
@@ -56,19 +57,17 @@ export function useHeader() {
     return () => {
       clearTimeout(timeoutID);
     };
-  }, [activeTask.isTimeActive, activeTask.totalTime]);
+  }, [isTimeActive, totalTime]);
 
-  const toggleTimer = () => token && taskHandler.toggleTimer(activeTask, dispatch, token);
-
-  const onKeyPress = (event: KeyboardEvent) => event.key === 'Enter' && toggleTimer();
-
-  const onInput = (event: ChangeEvent<HTMLInputElement>) =>
-    dispatch({ type: 'UPDATE_ACTIVE_TASK_NAME', payload: event.target.value });
+  const toggleTimer: () => void = useCallback(
+    () => token && taskHandler.toggleTimer(store.getState().activeTask, dispatch, token),
+    []
+  );
 
   return {
-    onInput,
+    isTimeActive,
+    name,
+    totalTime,
     toggleTimer,
-    activeTask,
-    onKeyPress,
   };
 }
