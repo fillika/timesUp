7c918808678584aac4b9d6c697b6ab0d62f6e3e0
A. Mironenko <alfie.front@gmail.com>
2021-07-17 12:31:06 +0300

    Добавил конфиги для email

--- utils/nodeMailer.js
+++ utils/nodeMailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 const AppError = require("./Error");
 
-let transporter = nodemailer.createTransport({
+const SMTP_BZ_CONFIG = {
   host: process.env.SMTP_HOST_BZ,
   port: process.env.SMTP_PORT_BZ,
   secure: false, // true for 465, false for other ports
@@ -9,7 +9,19 @@ let transporter = nodemailer.createTransport({
     user: process.env.SMTP_LOGIN_BZ, // generated ethereal user
     pass: process.env.SMTP_PASSWORD_BZ, // generated ethereal password
   },
-});
+}
+
+const SMTP_DEV_CONFIG = {
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USERNAME, // generated ethereal user
+    pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+  },
+}
+
+let transporter = nodemailer.createTransport(SMTP_DEV_CONFIG);
 
 exports.sendEmail = async (email, text, html) => {
   try {
