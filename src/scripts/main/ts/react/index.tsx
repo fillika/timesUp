97726773f785f4e770d7310abe8d7bd689d66558
A. Mironenko <alfie.front@gmail.com>
2021-07-19 12:43:59 +0300

    Добавил стили from MUI for sidebar

--- src/scripts/main/ts/react/index.tsx
+++ src/scripts/main/ts/react/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from 'Redux/reducers/rootReducer';
 import App from './App';
+import { store } from 'Redux/reducers/rootReducer';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#282c35',
+    },
+    secondary: {
+      main: '#6a5acd',
+    },
+    error: {
+      main: '#dc3545',
+    },
+    warning: {
+      main: '#fcc521'
+    },
+    success: {
+      main: '#28a745'
+    }
+  },
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('app')
 );
