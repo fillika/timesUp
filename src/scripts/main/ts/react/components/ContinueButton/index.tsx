095304e661f5b0fd16128c427a612ec6ec795ec0
A. Mironenko <alfie.front@gmail.com>
2021-07-19 13:54:19 +0300

    Заменил иконки для продолжения и остановки задачи

--- src/scripts/main/ts/react/components/ContinueButton/index.tsx
+++ src/scripts/main/ts/react/components/ContinueButton/index.tsx
@@ -1,20 +1,37 @@
 import React, { memo, useEffect } from 'react';
-import playBtn from 'Images/icons/play.svg';
 import { usePresenter } from './hooks/usePresenter';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import { IconButton } from '@material-ui/core';
 
 type ContinueButton = {
   name: string;
 };
 
+import { makeStyles, Theme } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme: Theme) => ({
+  iconButton: {
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+  },
+}));
+
 // memo стоит на true, чтобы кнопка продолжения не рендерилась при изменении имени таска
 export const ContinueButton = memo<ContinueButton>(({ name }) => {
   const [startTask, isTimeActive] = usePresenter(name);
+  const classes = useStyles();
 
   // useEffect(() => console.log('Render[ContinueButton]'));
 
   return (
-    <div onClick={startTask} className={`task-panel__icon task-panel__icon--play ${isTimeActive && 'disabled'}`}>
-      <img src={playBtn} alt='Продолжить задачу' />
-    </div>
+    <IconButton
+      onClick={startTask}
+      className={classes.iconButton}
+      color='secondary'
+      disabled={isTimeActive}
+      title='Продолжить задачу'>
+      <PlayArrowIcon />
+    </IconButton>
   );
 });
