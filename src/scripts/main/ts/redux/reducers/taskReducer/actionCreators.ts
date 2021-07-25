29a5832051933e5c35c67937ab7eb6521824d736
Aleksandr Mironenko <alfie.mir@gmail.com>
2021-07-25 22:47:12 +0300

    Перенес логику из TaskGandler в actionsCreator. Немного изменил её используя redux thunk

--- src/scripts/main/ts/redux/reducers/taskReducer/actionCreators.ts
+++ src/scripts/main/ts/redux/reducers/taskReducer/actionCreators.ts
@@ -1,12 +1,40 @@
 import { Dispatch } from 'react';
 import { RootState } from 'Redux/reducers/rootReducer';
 import { taskAPI } from 'Api/tasks';
+import { createNotify } from 'Redux/reducers/notifyReducer/actionCreators';
+import { activeTaskAPI } from 'Api/activeTask';
 
-const createTask = (payload: []) => ({ type: 'CREATE_TASK', payload: { newTask: payload } });
+const defaultData = {
+  at: 0,
+  name: '',
+  start: 0,
+  stop: 0,
+  duration: 0,
+  isTimeActive: false,
+  totalTime: '0:00:00',
+};
+
+const CREATE_TASK = 'CREATE_TASK',
+  RESET_ACTIVE_TASK_PROPS = 'RESET_ACTIVE_TASK_PROPS',
+  UPDATE_ACTIVE_TASK_START = 'UPDATE_ACTIVE_TASK_START',
+  UPDATE_ACTIVE_TASK_TIME = 'UPDATE_ACTIVE_TASK_TIME';
+
+const createTask = (payload: []) => ({ type: CREATE_TASK, payload: { newTask: payload } });
 
 const resetActiveTask = () => ({
-  type: 'RESET_ACTIVE_TASK_PROPS',
+  type: RESET_ACTIVE_TASK_PROPS,
   payload: { totalTime: '00:00:00', name: '', duration: 0 },
+});
+
+const updateActiveTaskStartTime = (start: number) => ({ type: UPDATE_ACTIVE_TASK_START, payload: start });
+const updateActiveTaskTotalTime = (start: number, end: number) => ({
+  type: UPDATE_ACTIVE_TASK_TIME,
+  payload: {
+    stop: end,
+    duration: end - new Date(start).getTime(),
+    at: end + 1000,
+    isTimeActive: false,
+  },
 });
 
 export const createTaskFetch = (token: string) => {
@@ -28,5 +56,38 @@ export const createTaskFetch = (token: string) => {
           break;
       }
     }
+  };
+};
+
+export const startTimer = () => {
+  return (dispatch: Dispatch<any>, getState: () => RootState) => {
+    const { activeTask } = getState();
+
+    if (activeTask.name.trim() === '') {
+      return dispatch(createNotify('warning', 'У задачи должно быть имя :)'));
+    }
+
+    const start = new Date().getTime();
+    dispatch(updateActiveTaskStartTime(start));
+  };
+};
+
+export const stopTimer = (token: string) => {
+  return async (dispatch: Dispatch<any>, getState: () => RootState) => {
+    const { activeTask } = getState();
+    const endTime = new Date().getTime();
+
+    dispatch(updateActiveTaskTotalTime(activeTask.start, endTime));
+    await activeTaskAPI.updateActiveTask(token, defaultData).catch(err => console.log(`Some err: ${err}`));
+  };
+};
+
+export const toggleTimer = (token: string) => {
+  return (dispatch: Dispatch<any>, getState: () => RootState) => {
+    const { activeTask } = getState();
+
+    !activeTask.isTimeActive
+      ? dispatch(startTimer())
+      : dispatch(stopTimer(token));
   };
 };
