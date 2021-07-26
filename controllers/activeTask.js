3c8664761f07f51698734aa562bf04afc12deeae
A. Mironenko <alfie.front@gmail.com>
2021-07-26 12:12:41 +0300

    Написал функцию утилиту, которая создает однотипные объекты успешного ответа с сервера

--- controllers/activeTask.js
+++ controllers/activeTask.js
@@ -1,6 +1,14 @@
 const { ActiveTask } = require("../models/activeTask");
 const asyncCatchHandler = require("../utils/asyncCatchHandler");
-const { mockErr} = require("../utils/mock/mockErr");
+const { mockErr } = require("../utils/mock/mockErr");
+
+const activeTaskResponseCreator = (message, data) => ({
+  status: "success",
+  message,
+  data: {
+    activeTask: data,
+  },
+})
 
 const getActiveTask = async (req, res, next) => {
   const { id } = req.user;
@@ -8,13 +16,7 @@ const getActiveTask = async (req, res, next) => {
   const result = await ActiveTask.findOne({ userID: id });
 
   //return mockErr(404, next);
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      activeTask: result,
-    },
-  });
+  res.status(200).json(activeTaskResponseCreator('Active task successful requested', result));
 };
 
 const updateActiveTask = async (req, res, next) => {
@@ -28,24 +30,11 @@ const updateActiveTask = async (req, res, next) => {
 
   if (result === null) {
     const createdTask = await createNewActiveTask(newBody);
-
-    res.status(200).json({
-      status: "success",
-      message: "Task has been created",
-      data: {
-        activeTask: createdTask,
-      },
-    });
+    res.status(200).json(activeTaskResponseCreator('Task has been created', createdTask));
     return;
   }
 
-  res.status(200).json({
-    status: "success",
-    message: "Task has been updated",
-    data: {
-      activeTask: result,
-    },
-  });
+  res.status(200).json(activeTaskResponseCreator('Task has been updated', result));
 };
 
 exports.getActiveTask = asyncCatchHandler(getActiveTask);
