ecb375a5849c690e5f32291468b26c8c1488223c
A. Mironenko <alfie.front@gmail.com>
2021-07-26 19:10:44 +0300

    Добавил стили с помощью styled component для инпута + селекта

--- src/scripts/main/react/components/Report/components/SearchForm/components/InputComponent/index.tsx
+++ src/scripts/main/react/components/Report/components/SearchForm/components/InputComponent/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { FormikProps } from 'formik';
 import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
-import { FormikProps } from 'formik';
+import styled from 'styled-components';
+
+const StyledSearchInput = styled.div`
+  display: flex;
+  width: 100%;
+  padding-left: 16px;
+  margin-top: 10px;
+
+  .MuiTextField-root {
+    width: 100%;
+  }
+
+  .MuiInputLabel-root {
+  }
+`;
 
 export const InputComponent: React.FC<{
   formik: FormikProps<{
@@ -11,7 +26,7 @@ export const InputComponent: React.FC<{
   }>;
 }> = ({ formik }) => {
   return (
-    <div>
+    <StyledSearchInput>
       <TextField
         name='nameOfTask'
         value={formik.values.nameOfTask}
@@ -21,6 +36,6 @@ export const InputComponent: React.FC<{
       <IconButton type='submit'>
         <SearchIcon />
       </IconButton>
-    </div>
+    </StyledSearchInput>
   );
 };
--- src/scripts/main/react/components/Report/components/SearchForm/components/SelectComponent/index.tsx
+++ src/scripts/main/react/components/Report/components/SearchForm/components/SelectComponent/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { FormikProps } from 'formik';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import DateRangeIcon from '@material-ui/icons/DateRange';
-import { FormikProps } from 'formik';
+import IconButton from '@material-ui/core/IconButton';
+import styled from 'styled-components';
+
+const StyledSelectField = styled.div`
+  display: flex;
+
+  .select {
+    width: 100%;
+    max-width: 450px;
+  }
+`;
 
 export const SelectComponent: React.FC<{ formik: FormikProps<{ nameOfTask: string; date: string }> }> = ({
   formik,
@@ -10,18 +21,18 @@ export const SelectComponent: React.FC<{ formik: FormikProps<{ nameOfTask: strin
   const selectOptions = ['Today', 'This week', 'Last week', 'This month', 'Last month', 'This year', 'Last year'];
 
   return (
-    <div>
-      <div>
+    <StyledSelectField>
+      <IconButton>
         <DateRangeIcon />
-      </div>
+      </IconButton>
 
-      <Select name='date' value={formik.values.date} onChange={formik.handleChange}>
+      <Select name='date' value={formik.values.date} onChange={formik.handleChange} className="select">
         {selectOptions.map((val, index) => (
           <MenuItem value={val} key={index}>
             {val}
           </MenuItem>
         ))}
       </Select>
-    </div>
+    </StyledSelectField>
   );
 };
