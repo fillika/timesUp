62c12394ec087b2b30b0ebf48405d910e60734f9
Aleksandr Mironenko <alfie.mir@gmail.com>
2021-07-26 22:12:32 +0300

    Доработал АПИ. Добавил обработку ошибок + regExp для имени

--- controllers/report.js
+++ controllers/report.js
@@ -1,18 +1,34 @@
 
 const { TaskModel } = require("../models/task");
+const AppError = require("../utils/Error");
 const asyncCatchHandler = require("../utils/asyncCatchHandler");
 
 exports.getStats = asyncCatchHandler(getStats)
 
 async function getStats(req, res, next) {
-  const { name, date } = req.query;
+  const { name, start, stop } = req.query;
+
+  if (!name || !start || !stop) {
+    return next(new AppError('You must to provide name, start and stop params', 400))
+  }
+
+  const queryObj = {
+    userID: req.user._id,
+    name: new RegExp(name, 'gi'),
+    $and: [
+      { start: { $gte: start } },
+      { stop: { $lte: stop } }
+    ]
+  }
+
+  const result = await TaskModel.find(queryObj).sort({ at: "desc" });
 
   res.status(200).json({
     status: "success",
     message: "getStats",
     data: {
-      name,
-      date
+      queryObj,
+      result
     }
   })
 }
