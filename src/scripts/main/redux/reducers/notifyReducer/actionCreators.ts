25224f782a9e01a50ed87dbc937d9a93e4df4a2e
A. Mironenko <alfie.front@gmail.com>
2021-07-27 13:33:58 +0300

    Добавил уведомление если вдруг ничего не найдено по запросу. Изменил время уведомления по умолчанию на 3500 миллисек

--- src/scripts/main/redux/reducers/notifyReducer/actionCreators.ts
+++ src/scripts/main/redux/reducers/notifyReducer/actionCreators.ts
@@ -4,7 +4,7 @@ import { RootState } from 'Redux/reducers/rootReducer';
 
 export const SET_NOTIFICATION = 'SET_NOTIFICATION';
 
-export const createNotify = (type: string, message: string, time: number = 2200) => {
+export const createNotify = (type: string, message: string, time: number = 3500) => {
   return (dispatch: Dispatch<any>, getState: () => RootState) => {
     dispatch({
       type: SET_NOTIFICATION,
--- src/scripts/main/redux/reducers/reportReducer/middlewares.ts
+++ src/scripts/main/redux/reducers/reportReducer/middlewares.ts
@@ -7,13 +7,16 @@ import { addReportsResult } from './actionCreators';
 import { errSwitchCase } from 'Utils/helpers/errSwitchCase';
 import { AppError } from 'Utils/Error';
 import { ReportResponse, ServerResponse } from 'Types/serverResponse';
+import { createNotify } from 'Redux/reducers/notifyReducer/actionCreators';
 
-// Todo типизировать Response
 export const getReportResult = (token: string, params: ReportsFetchParams) => {
   return (dispatch: Dispatch<any>, getState: () => RootState) => {
     reportsAPI
       .getReports(token, params)
       .then((response: ServerResponse<ReportResponse>) => {
+        const message = 'Ничего не найдено по Вашему запросу. Попробуйте другой временной интервал или другое имя.';
+        if (response.data.task.length === 0) dispatch(createNotify('warning', message));
+
         dispatch(addReportsResult(sort.sortReports(response.data.task)));
       })
       .catch((err: AppError) => errSwitchCase(err, dispatch));
