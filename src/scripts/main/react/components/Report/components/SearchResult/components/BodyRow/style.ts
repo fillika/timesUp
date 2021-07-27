f358ad179ba0f2dc27d7db8133e1f0f11778de99
A. Mironenko <alfie.front@gmail.com>
2021-07-27 15:44:28 +0300

    Small fix styles and HTML

--- src/scripts/main/react/components/Report/components/SearchResult/components/BodyRow/style.ts
+++ src/scripts/main/react/components/Report/components/SearchResult/components/BodyRow/style.ts
@@ -4,10 +4,10 @@ import TableCell from '@material-ui/core/TableCell';
 export const StyledCellTime = styled(TableCell)`
   width: 15%;
   min-width: 90px;
-  font-size: 1.15em;
+  font-size: .95em;
 `;
 
 export const StyledCellName = styled(TableCell)`
   width: 85%;
-  font-size: 1.15em;
+  font-size: .95em;
 `;
