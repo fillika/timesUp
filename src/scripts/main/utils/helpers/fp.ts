9091f026700e6f802742cd1a447ac6d212324970
A. Mironenko <alfie.front@gmail.com>
2021-07-29 18:55:05 +0300

    Добавил функцию каррирования

--- src/scripts/main/utils/helpers/fp.ts
+++ src/scripts/main/utils/helpers/fp.ts
@@ -8,4 +8,10 @@ export const pipe =
   (argument: any) =>
     fns.reduce((prev, next) => next(prev), argument);
 
+export const curry = (fn: any) =>
+  function curried(this: any, ...args: any[]) {
+    if (args.length >= fn.length) return fn.apply(this, args);
+    return (...args2: any[]) => curried.apply(this, args.concat(args2));
+  };
+
 export const createDeepCopy = <T, Y>(data: T): Y => JSON.parse(JSON.stringify(data));
