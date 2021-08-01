0c51e5cdb3dce1e8d587b2d8c59961a60b310c9a
Aleksandr Mironenko <alfie.mir@gmail.com>
2021-08-01 14:29:01 +0300

    Правда бага со временем. Неверно высчитывались интервалы между началом и концом недели и прошлого года

--- src/scripts/main/utils/Date.ts
+++ src/scripts/main/utils/Date.ts
@@ -1,24 +1,35 @@
 const ONE_DAY_LENGTH = 86400000;
 
 export const startOfDay = (date: string | number): number => {
-  const [currentDay] = new Date(date).toISOString().split('T');
-  return new Date(currentDay).getTime();
+  // const [currentDay] = new Date(date).toISOString().split('T');
+  // return new Date(currentDay).getTime();
+  const dateString = new Date(date).toDateString();
+  return new Date(dateString).getTime();
 };
 export const endOfDay = (date: number) => date + ONE_DAY_LENGTH;
 export const getToday = () => ({
   start: startOfDay(new Date().getTime()),
   stop: endOfDay(startOfDay(new Date().getTime())),
 });
+
 export const getCurrentWeek = () => {
   // Номер понедельника на неделе - 1. Надо найти понедельник, а потом прибавить к нему 7 дней.
   const currentDay = startOfDay(new Date().getTime());
-  const monday = currentDay - (new Date(currentDay).getDay() - 1) * ONE_DAY_LENGTH; // Получаем начало дня ПН
-  const sunday = monday + ONE_DAY_LENGTH * 7; // Это начало след понедельника (по сути конец воскресенья)
-
-  return {
-    start: monday,
-    stop: sunday,
+  const dayNumber = new Date(currentDay).getDay();
+  const result = {
+    start: 0,
+    stop: 0,
   };
+
+  if (dayNumber === 0) {
+    result.stop = new Date(1627765200000 + 86400000).getTime();
+    result.start = result.stop - ONE_DAY_LENGTH * 7;
+  } else {
+    result.start = currentDay - (dayNumber - 1) * ONE_DAY_LENGTH;
+    result.stop = result.start + ONE_DAY_LENGTH * 7;
+  }
+
+  return result;
 };
 export const getLastWeek = ({ start }: { start: number }) => ({
   start: start - ONE_DAY_LENGTH * 7,
@@ -51,8 +62,8 @@ export const getCurrentYear = () => {
 };
 export const getLastYear = () => {
   const firstDayOfCurrentMonth = new Date(startOfDay(new Date().getTime())).setDate(1);
-  const startOfYear = new Date(firstDayOfCurrentMonth).setMonth(0);
-  const endOfYear = new Date(firstDayOfCurrentMonth).setMonth(-12);
+  const endOfYear = new Date(firstDayOfCurrentMonth).setMonth(0);
+  const startOfYear = new Date(firstDayOfCurrentMonth).setMonth(-12);
   return {
     start: startOfYear,
     stop: endOfYear,
