3a2306895cd2f0a550583ec2e19e6c5fd0be3e30
A. Mironenko <alfie.front@gmail.com>
2021-08-03 17:00:06 +0300

    Немного сократил код, вынес функцию проверки длины в общие утилиты

--- src/scripts/main/react/components/DatePickerComponent/components/TimeInput/hooks/usePresenter.tsx
+++ src/scripts/main/react/components/DatePickerComponent/components/TimeInput/hooks/usePresenter.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { isValid } from './../isValid';
 import { createDeepCopy } from 'Utils/helpers/createDeepCopy';
+import { checkLength } from 'Utils/helpers/checkLength';
 import flow from 'lodash/fp/flow';
 import curry from 'lodash/fp/curry';
 
@@ -13,14 +14,7 @@ export const usePresenter: TPresenter = (initTime = '', onTimeChange) => {
   const [time, setTime] = useState(isValid(initTime) ? initTime : '');
   const [lastVal, setLastVal] = useState('');
 
-  const checkFirstChar = (val: string) => {
-    if (Number(val[0]) > 2) val = '2'; // Первый символ - максимум 2
-    return val;
-  };
-
-  // Проверка на длину
-  const checkLength = (val: string | [], number: number) => (val.length === number ? true : false);
-
+  const checkFirstChar = (value: string) => (Number(value[0]) > 2 ? (value = '2') : value);
   const addColon = (value: string) => {
     if (value.length > 5) return false;
     if (checkLength(value, 2) && !checkLength(lastVal, 3) && value.indexOf(':') === -1) {
---
+++ src/scripts/main/utils/helpers/checkLength.ts
@@ -0,0 +1,1 @@
+export const checkLength = (value: string | [], number: number) => (value.length === number ? true : false);
