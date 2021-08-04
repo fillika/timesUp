79b410412c54ab6a415d61a04460a2ba33bc8c03
A. Mironenko <alfie.front@gmail.com>
2021-08-04 14:21:35 +0300

    Добавил доп тип для лучшей читаемости

--- src/scripts/main/react/components/DatePickerComponent/components/TimeInput/utils/checkValueValidation.ts
+++ src/scripts/main/react/components/DatePickerComponent/components/TimeInput/utils/checkValueValidation.ts
@@ -1,6 +1,6 @@
 import curry from 'lodash/fp/curry';
 import { isValid } from './isValid';
 
-export const checkValueValidation = curry(
-  (setState: (value: string) => void, value: string) => isValid(value) && setState(value)
-);
+type TSetState = (value: string) => void;
+
+export const checkValueValidation = curry<TSetState, string, void>((setState, value) => isValid(value) && setState(value));
