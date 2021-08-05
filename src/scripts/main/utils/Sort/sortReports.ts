12a4b88917f6118df45804692747d453cc3e8062
A. Mironenko <alfie.front@gmail.com>
2021-08-05 17:33:26 +0300

    Немного сократил код. Удалил лишние строчки

--- src/scripts/main/utils/Sort/sortReports.ts
+++ src/scripts/main/utils/Sort/sortReports.ts
@@ -1,35 +1,26 @@
 import uniqueId from 'lodash/uniqueId';
 import clone from 'ramda/src/clone';
 import compose from 'ramda/src/compose';
-import path from 'ramda/src/path';
 import { ExtendedDBTask, SortedReport } from 'Redux/reducers/reportReducer/types';
 import { DatabaseTask } from 'Types/tasks';
 
 // Сгрупировать таски с одинаковым именем
 const groupDatabaseTask = (arr: ExtendedDBTask[]) => {
-  const result: SortedReport = {};
-
-  
-  arr.forEach(task => {
+  return arr.reduce((result: SortedReport, currentDBTask: ExtendedDBTask) => {
     // Найти есть ли подобный таск в массиве с именем
-    if (result[task.name] === undefined) {
-      // path([task.name], result)
-      result[task.name] = {
-        taskList: [],
-        total: 0,
-        // Create uniq name for each group task
+    if (result[currentDBTask.name] === undefined) {
+      result[currentDBTask.name] = {
+        taskList: [currentDBTask],
+        total: currentDBTask.duration,
         name: uniqueId('task-name-'),
       };
-      result[task.name].taskList = [];
-      result[task.name].total += task.duration;
-      result[task.name].taskList.push(task);
     } else {
-      result[task.name].total += task.duration;
-      result[task.name].taskList.push(task);
+      result[currentDBTask.name].total += currentDBTask.duration;
+      result[currentDBTask.name].taskList.push(currentDBTask);
     }
-  });
 
-  return result;
+    return result;
+  }, {})
 };
 
 // Меняю местами имя таска и его уникальный ключ name, который будет использоваться как часть URL
