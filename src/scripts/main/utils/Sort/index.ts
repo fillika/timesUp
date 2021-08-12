a8630dee9016587aa0e167a3acf919e983921855
A. Mironenko <alfie.front@gmail.com>
2021-08-12 15:34:47 +0300

    Вынес функции в отдельные методы внутри класса Sort

--- src/scripts/main/utils/Sort/index.ts
+++ src/scripts/main/utils/Sort/index.ts
@@ -1,10 +1,14 @@
 import { SortedTask, TaskType, DatabaseTask } from 'Types/tasks';
+import head from 'ramda/src/head';
+import last from 'ramda/src/last';
 import clone from 'ramda/src/clone';
 import reduce from 'ramda/src/reduce';
+import map from 'ramda/src/map';
 import compose from 'ramda/src/compose';
 import findIndex from 'ramda/src/findIndex';
 import prop from 'ramda/src/prop';
 import propEq from 'ramda/src/propEq';
+import sortBy from 'ramda/src/sortBy';
 import sortWith from 'ramda/src/sortWith';
 import descend from 'ramda/src/descend';
 import { calcDuration } from 'Utils/Date';
@@ -19,30 +23,10 @@ class Sort {
     const propAt = prop('at');
     const sortDESCByAt = sortWith([descend<DatabaseTask>(propAt)]);
 
-    const reduceIterator = (result: SortedTask[], el: DatabaseTask) => {
-      const date = new Date(el.at).toLocaleDateString();
-
-      // Тут Я создаю самый первый таск
-      if (!result.length) {
-        const sortedTask = this.createFirstSortedTask(el);
-        result.push(sortedTask);
-      } else {
-        // Ищу таски с одинаковой датой
-        const index = findIndex(propEq('date', date))(result);
-
-        if (index === -1) {
-          const sortedTask = this.createFirstSortedTask(el);
-          result.push(sortedTask);
-        } else {
-          this.findDuplicatesAndPush(result[index].mainTaskList, el);
-        }
-      }
-
-      return result;
-    };
-
-    const reducedList = reduce(reduceIterator, []);
-    const sortList = compose(reducedList, sortDESCByAt, clone);
+    const sortWithReduce = reduce(this.reduceIteratorSort.bind(this), []);
+    const modifyStartAndStop = map(this.mapModifyStartAndStop);
+    
+    const sortList = compose(modifyStartAndStop, sortWithReduce, sortDESCByAt, clone);
 
     return sortList(taskArr);
   }
@@ -68,10 +52,7 @@ class Sort {
 
     if (index !== -1) {
       if (task.time === undefined) {
-        task.time = [
-          this.createTask(task),
-          this.createTask(el)
-        ];
+        task.time = [this.createTask(task), this.createTask(el)];
 
         task.duration += el.duration;
       } else {
@@ -90,6 +71,56 @@ class Sort {
       stop: el.stop,
       duration: calcDuration(el.stop, el.start),
     };
+  }
+
+  reduceIteratorSort(result: SortedTask[], el: DatabaseTask) {
+    const date = new Date(el.at).toLocaleDateString();
+    
+    // Тут Я создаю самый первый таск
+    if (!result.length) {
+      const sortedTask = this.createFirstSortedTask(el);
+      result.push(sortedTask);
+    } else {
+      // Ищу таски с одинаковой датой
+      const index = findIndex(propEq('date', date))(result);
+
+      if (index === -1) {
+        const sortedTask = this.createFirstSortedTask(el);
+        result.push(sortedTask);
+      } else {
+        this.findDuplicatesAndPush(result[index].mainTaskList, el);
+      }
+    }
+
+    return result;
+  }
+
+  mapModifyStartAndStop(task: SortedTask) {
+    const mainTaskList = prop('mainTaskList', task);
+
+    /**
+     * В этой функции у родительского таска Я перезаписываю свойства start и стоп,
+     * чтобы правильно показать начало и конец (если тасков в течении дня несколько)
+     */
+    const mapTaskList = (el: TaskType) => {
+      if (el.time) {
+        const startProp = prop('start');
+        const stopProp = prop('stop');
+
+        const sortByStart = sortBy(startProp);
+        const sortByStop = sortBy(stopProp);
+
+        const getStart = head(sortByStart(el.time));
+        const getStop = last(sortByStop(el.time));
+
+        if (getStart) el.start = startProp(getStart);
+        if (getStop) el.stop = stopProp(getStop);
+      }
+    };
+
+    map(mapTaskList, mainTaskList);
+
+    return task;
   }
 }
 export const sort = new Sort();
